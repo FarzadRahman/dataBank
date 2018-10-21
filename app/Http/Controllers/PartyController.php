664cf40c8d628afe88ba22090cd9a38df10aec40
mdsak <md.sakibrahman@hotmail.com>
2018-10-21 22:16:52 +0600

    sakib

--- app/Http/Controllers/PartyController.php
+++ app/Http/Controllers/PartyController.php
@@ -10,4 +10,37 @@ class PartyController extends Controller
         return view('party.index');
     }
 
+
+    public function insert(Request $r){
+        $validatedData = $r->validate([
+            'divisionName' => 'required|unique:division,divisionName|max:45'
+        ]);
+        $division=new Division();
+        $division->divisionName=$r->divisionName;
+        $division->createdBy=Auth::user()->userId;
+        $division->save();
+        Session::flash('message', 'Division Added Successfully!');
+        return back();
+
+    }
+
+    public function edit(Request $r){
+        $division=Division::findOrFail($r->id);
+
+        return view('division.edit',compact('division'));
+    }
+
+    public function update(Request $r,$id){
+
+        $validatedData = $r->validate([
+            'divisionName' => 'required|unique:division,divisionName,'.$id.',divisionId|max:45'
+        ]);
+        $division=Division::findOrFail($id);
+        $division->divisionName=$r->divisionName;
+        $division->updatedBy=Auth::user()->userId;
+        $division->save();
+        Session::flash('message', 'Division Updated Successfully!');
+        return back();
+    }
+
 }
--- resources/views/party/index.blade.php
+++ resources/views/party/index.blade.php
@@ -1,5 +1,69 @@
 @extends('main')
 @section('content')
+
+    <div class="modal" id="myModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Add Party</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body">
+                    <form method="post" action="{{route('party.insert')}}">
+                        {{csrf_field()}}
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <label>Party</label>
+                                <input type="text" name="partyName" placeholder="party name" class="form-control">
+                            </div>
+                            <div class="form-group col-md-12">
+                                <button class="btn btn-success pull-right">Insert</button>
+                            </div>
+
+                        </div>
+                    </form>
+
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <!-- The Edit Modal -->
+    <div class="modal" id="editModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Add Division</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body" id="editModalBody">
+
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
     <div class="container">
         <div class="card">
             <div class="card-header">
